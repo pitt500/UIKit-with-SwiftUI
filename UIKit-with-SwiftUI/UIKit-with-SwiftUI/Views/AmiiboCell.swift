9f147b260faa7dfc588f538792fb6be89b7fd5ef
pitt500 <pedro.rojaslo50@gmail.com>
2019-12-16 16:02:53 -0700

    Add Disclosure indicator button

--- UIKit-with-SwiftUI/UIKit-with-SwiftUI/Views/AmiiboCell.swift
+++ UIKit-with-SwiftUI/UIKit-with-SwiftUI/Views/AmiiboCell.swift
@@ -32,6 +32,7 @@ class AmiiboCell: UITableViewCell {
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
     selectionStyle = .none
+    accessoryType = .disclosureIndicator
     setupImageView()
     setupNameLabel()
     setupPriceLabel()
