1c6f1562db73eebc0b05c10de5e44066cfb93ded
pitt500 <pedro.rojaslo50@gmail.com>
2019-12-16 10:16:07 -0700

    First commit

---
+++ UIKit-with-SwiftUI/UIKit-with-SwiftUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIKit-with-SwiftUI
+//
+//  Created by projas on 12/16/19.
+//  Copyright © 2019 projas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
